09feb40c1eea3156f5dd20070b20fe933efdd8e3
RajRampragash <rajrampragash0208@gmail.com>
2024-02-21 16:07:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./components/about";
+import Home from "./components/home";
+import Navbar from "./components/navbar";
+
+import { Routes, Route, useNavigate } from "react-router-dom";
+import NoteState from "./context/notes/noteState";
+import Alert from "./components/alert";
+import { useContext, useEffect, useState } from "react";
+import Loginpage from "./components/loginpage";
+import Signup from "./components/signup";
+import MyContext from "../src/context/notes/noteContext";
 
 function App() {
+  // creating a state for alert message
+  const [alertstatus, SetAlertStatus] = useState(true);
+  function status() {
+    {
+      setTimeout(() => {
+        SetAlertStatus(false);
+      }, 1000);
+    }
+  }
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* for using context api  */}
+      <NoteState>
+        {/* your normal code  */}
+        <Navbar />
+        <Alert message={"Hi, this is a simple alert message"} />;
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Loginpage />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </NoteState>
+    </>
   );
 }
 
